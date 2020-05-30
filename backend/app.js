3ae660c8726317922daaf6d50b56b8cabc287822
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-05-30 19:29:36 -0400

    node websockets

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
+const WebSocket = require('ws');
+const http = require('http');
 require('dotenv/config');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//WebSocket
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
+wss.on('connection', (ws) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
+});
+
+
+
+
+
+
+
+//API
 const loginRoutes = require('./routes/login');
 const roomRoutes = require('./routes/rooms');
 //Routes
@@ -15,10 +45,11 @@ app.get('/',(req,res) =>{
     res.send("We are on home");
 });
 
+
 mongoose.connect(process.env.DB_CONNECTION,    { useNewUrlParser: true },
     () => {
     console.log("connected to db");
 },
-
 );
-app.listen(3000)
+app.listen(3000);
+
