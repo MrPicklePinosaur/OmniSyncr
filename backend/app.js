2351706df3e90884f4371de1d2116ac8216b430e
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-05-30 19:47:00 -0400

    js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
 const WebSocket = require('ws');
 const http = require('http');
 require('dotenv/config');
@@ -8,26 +7,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //WebSocket
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocket.Server({server});
+wss.on('connection',function connection(ws){
+    ws.on('message',function incoming(data){
+        wss.clients.forEach(function each(client){
+            if(client!=ws && client.readyState === WebSocket.OPEN);
+        })
+    })
 
-wss.on('connection', (ws) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-    //send immediatly a feedback to the incoming connection
-    ws.send('Hi there, I am a WebSocket server');
 });
 
-//start our server
-server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
-});
 
 
 
