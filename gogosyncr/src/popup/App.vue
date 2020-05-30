7654b4b582ed796e123dc63923fc93a0db9e144e
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-30 18:52:48 -0400

    router and views

--- gogosyncr/src/popup/App.vue
+++ gogosyncr/src/popup/App.vue
@@ -1,19 +1,39 @@
 <template>
-  <div>
-    <p>Hello world!</p>
-  </div>
+	<div style="width: 250px; height: 400px;">
+		
+		<div class="container">
+			<h1>gogoSyncr</h1>
+		</div>
+
+		<nav class="navbar">
+
+			<ul class="navbar-nav">
+
+				<li><router-link to="/">Home</router-link></li>
+				<li><router-link to="/test">Test</router-link></li>
+				<li><router-link to="/room">Room</router-link></li>
+
+			</ul>
+
+		</nav>
+
+		<router-view></router-view>
+
+	</div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {}
-  }
+	data: function () {
+		return {
+
+		}
+	}
 }
 </script>
 
 <style lang="scss" scoped>
 p {
-  font-size: 20px;
+	font-size: 20px;
 }
 </style>
