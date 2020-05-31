88d21c439a0e3e55e231c523b384be89f7eb7fe5
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-05-30 22:32:31 -0400

    big pp

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const WebSocket = require('ws');
 const http = require('http');
 require('dotenv/config');
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //WebSocket
