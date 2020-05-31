fa4e1d8064f2f9be0dd13783700cb0bd144338f3
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-05-31 03:22:38 -0400

    more requests lmaoxdd

--- gogosyncr/src/popup/store/store.js
+++ gogosyncr/src/popup/store/store.js
@@ -30,26 +30,46 @@ export const store = new Vuex.Store({
         },
         
         setRoom: (state, payload) => {
+            console.log(payload);
             state.room_info = payload; //copy the room object over
         }
     },
     actions: {
 
-        createRoom: (content) => { 
-            axios.get(context.server_url+"/rooms/create")
-                .then(response => { //response: room object { code: string, dbCode: string, owner: string}
-                    context.commit('setRoom', response);
+        createRoom: context => { 
+            console.log(context.state.server_url+"/rooms/create");
+            /*
+            axios.get(context.state.server_url+"/rooms/create") //response: room object { code: string, dbCode: string, owner: string}
+            .then(response => {
+                console.log(response);
+                context.commit('setRoom', response)
+            });
+            */
+            setTimeout(function() { //dummy request for now
+                context.commit('setRoom', {
+                    "code": "lmaoxdcode",
+                    "dbCode": "123securehash", //firestore
+                    "owner": "melmao",
                 });
+            },100);
         },
 
-        joinRoom: (content, payload) => { //payload: object: { code: string }
+        joinRoom: (context, payload) => { //payload: object: { code: string }
 
             //send code we entered to the server and check to see if we successfully joined room
 
-            axios.get(context.server_url+"/rooms/getroom")
-                .then(response => { //response: room object
+            axios.get(context.server_url+"/rooms/getroom", {
+                "code": payload.code
+            })
+            .then(response => { //response: room object OR null
 
-                });
+                if (repsonse != null) {
+                    context.commit('setRoom', response);
+                } else {
+                    //tell user that its an invalid room
+                }
+
+            });
         },
 
         //leaveRoom:
